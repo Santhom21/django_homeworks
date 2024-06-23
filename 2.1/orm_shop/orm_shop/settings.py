881ae1b9074bfac1e89f19f90e671bf7c29bfc9c
Santhom <santhom1999@icloud.com>
2024-06-23 21:09:49 +0300

    change creds

--- 2.1/orm_shop/orm_shop/settings.py
+++ 2.1/orm_shop/orm_shop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '752261Santhom',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': 5432
     }
